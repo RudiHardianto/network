886c7a150d6d5621cc8259c561b55f4695ed93c9
Rudi Hardianto <rud.hardianto@gmail.com>
2021-11-29 08:31:07 +0700

    perbaiki factory dan identifier (prefix)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -38,11 +38,14 @@
             </x-slot>
 
             <x-slot name="content">
+              <x-dropdown-link href="{{ route('profile', Auth::user()->username) }}">
+                Profile
+              </x-dropdown-link>
               <!-- Authentication -->
               <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
-                                      this.closest('form').submit();">
+                     this.closest('form').submit();">
                   {{ __('Log Out') }}
                 </x-dropdown-link>
               </form>
@@ -93,11 +96,14 @@
         </div>
 
         <div class="mt-3 space-y-1">
+          <x-responsive-nav-link href="{{ route('profile', Auth::user()->username) }}">
+            Profile
+          </x-responsive-nav-link>
           <!-- Authentication -->
           <form method="POST" action="{{ route('logout') }}">
             @csrf
             <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
-                                this.closest('form').submit();">
+                     this.closest('form').submit();">
               {{ __('Log Out') }}
             </x-responsive-nav-link>
           </form>
